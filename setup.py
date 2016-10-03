64d6514553a81e64f708be5c8e2110e38148d937
Raphael Baron <rbaron@rbaron.net>
2016-10-03 10:13:12 -0300

    Bumped version to 2.0.1 and pushed new version to pypi.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
       name="dict.cc.py",
       description="Unofficial dict.cc command line interface",
-      version="2.0",
+      version="2.0.1",
       url="http://github.com/rbaron/dict.cc.py",
       install_requires=["beautifulsoup4"],
       packages=find_packages(),
