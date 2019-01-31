fdfd77a10f20f6c4467d34b6cbd3962b82ff250a
rbaron <rbaron@rbaron.net>
2019-01-31 20:18:43 +0100

    Bumped version to 3.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
       name="dict.cc.py",
       description="Unofficial dict.cc command line interface",
-      version="2.0.1",
+      version="3.0.0",
       url="https://github.com/rbaron/dict.cc.py",
       install_requires=["beautifulsoup4","requests"],
       packages=find_packages(),
