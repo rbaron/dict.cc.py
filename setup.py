675d683d6e4eb8738b5f806530a2adad51d60dd0
rbaron <rbaron@rbaron.net>
2021-05-02 21:06:58 +0200

    Bumps version to 3.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
       name="dict.cc.py",
       description="Unofficial dict.cc command line interface",
-      version="3.0.0",
+      version="3.1.0",
       url="https://github.com/rbaron/dict.cc.py",
       install_requires=["beautifulsoup4","requests","colorama"],
       packages=find_packages(),
